1bde02910f4a909dbd32125e62449b41fb06014e
subbni <slfkak@naver.com>
2024-09-01 13:17:52 +0900

    [#1] Feat: 로그인 api 작성
    -  Controller / Service 단 분리

---
+++ backend/src/services/authService.js
@@ -0,0 +1,35 @@
+import AuthErrorMessage from '../constants/error/authErrorMessage.js';
+import MemberModel from '../models/memberModel.js';
+
+class AuthService {
+	static async registerMember({ email, password, nickname }) {
+		// check if email is already taken
+		if ((await MemberModel.findMemberByEmail(email)) !== null) {
+			throw new Error(AuthErrorMessage.DUPLICATED_EMAIL);
+		}
+
+		const result = await MemberModel.createMember({
+			email,
+			password,
+			nickname,
+		});
+
+		return result;
+	}
+
+	static async login({ email, password }) {
+		const member = await MemberModel.findMemberByEmail(email);
+		if (member === null) {
+			throw new Error(AuthErrorMessage.EMAIL_NOT_FOUND);
+		}
+		if (member.password !== password) {
+			throw new Error(AuthErrorMessage.INVALID_PASSWORD);
+		}
+		return {
+			email: member.email,
+			nickname: member.nickname,
+		};
+	}
+}
+
+export default AuthService;

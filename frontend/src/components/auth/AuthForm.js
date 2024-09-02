8a61ba5b5e9b2491862e8b7025c6a7225ba29075
subbni <slfkak@naver.com>
2024-09-02 19:18:21 +0900

    [#3] Design : Or 컴포넌트 생성
    - SNS 로그인 UI를 위한 Or 컴포넌트 생성 및 적용

--- frontend/src/components/auth/AuthForm.js
+++ frontend/src/components/auth/AuthForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
+import Or from '../common/Or';
 
 const AuthFormBlock = styled.div`
 	text-align: center;
@@ -35,7 +36,7 @@ const authButtonStyle = css`
 	background-color: var(--color-light-gray);
 	color: var(--color-dark-gray);
 	padding: 0.5rem;
-	margin-top: 0.5rem;
+	margin-top: 1rem;
 	font-size: 1rem;
 `;
 
@@ -73,6 +74,7 @@ const AuthForm = ({ type }) => {
 				<StyledInput placeholder="비밀번호" />
 				{type === '회원가입' && <StyledInput placeholder="비밀번호 확인" />}
 				<StyledButton>확인</StyledButton>
+			</form>
 				<AuthFooter>
 					{type === '회원가입' ? (
 						<Link to="/login">이미 계정이 있으신가요?</Link>
@@ -84,7 +86,7 @@ const AuthForm = ({ type }) => {
 						</>
 					)}
 				</AuthFooter>
-			</form>
+			<Or />
 		</AuthFormBlock>
 	);
 };

7f8f492aca851cbb3a2b883c6352b9233f334f76
subbni <slfkak@naver.com>
2024-09-03 16:56:56 +0900

    [#5] Feat : 토큰 검증 & 로그아웃 api 작성

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -5,7 +5,7 @@ class AuthController {
 	static async registerMember(req, res) {
 		const { email, password, nickname } = req.body;
 		// check required fields
-		if ([email, password, nickname].includes('')) {
+		if (!email || !password || !nickname) {
 			res.status(400);
 			return;
 		}
@@ -25,12 +25,16 @@ class AuthController {
 	static async login(req, res) {
 		const { email, password } = req.body;
 		// check required fields
-		if ([email, password].includes('')) {
-			res.status(400);
-			return;
+		if (!email || !password) {
+			res.status(400).json({ message: 'email and password are required' });
 		}
 		try {
 			const data = await AuthService.login({ email, password });
+			const token = AuthService.generateToken(data);
+			res.cookie('access_token', token, {
+				maxAge: 1000 * 60 * 60 * 24 * 7,
+				httpOnly: true,
+			});
 			data.message = '로그인 되었습니다.';
 			res.status(201).json(data);
 		} catch (e) {
@@ -38,9 +42,20 @@ class AuthController {
 		}
 	}
 
-	static async check(req, res) {}
+	static async check(req, res) {
+		const { member } = req.state;
+		if (!member) {
+			res.status(401).end(); // UNAUTHORIZED
+		}
+		res.json(member);
+	}
 
-	static async logout(req, res) {}
+	static async logout(req, res) {
+		res.clearCookie('access_token', req.cookies.access_token, {
+			httpOnly: true,
+		});
+		res.status(204).end(); // NO_CONTENT
+	}
 }
 
 export default AuthController;
---
+++ backend/src/lib/jwtMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const jwtMiddleware = async (req, res, next) => {
+	const token = req.cookies.access_token;
+	req.state = req.state || {};
+
+	if (!token) next();
+
+	try {
+		// 토큰 검증
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.state.member = {
+			member_id: decoded.member_id,
+			nickname: decoded.nickname,
+		};
+		// TODO : 만료 기간이 기준일 미만이라면 재발급 처리
+	} catch (e) {
+		// 검증 실패
+	}
+	next();
+};
+
+export default jwtMiddleware;

1ed29f8e87d4da784860e7d6df7367ad34c21345
subbni <slfkak@naver.com>
2024-09-18 13:13:49 +0900

    Feat : CustomError 구현
    - 각 ErrorMessage 안에 넘겨줄 error message와 status를 함께 저장하도록 수정
    - ErrorMessage를 인수로 받는 커스텀 Error 구현

--- backend/src/constants/error/authErrorMessage.js
+++ backend/src/constants/error/authErrorMessage.js
@@ -1,7 +1,13 @@
 const AuthErrorMessage = {
-	DUPLICATED_EMAIL: '해당 이메일로 가입된 계정이 이미 존재합니다.',
-	EMAIL_NOT_FOUND: '해당 이메일로 가입된 계정이 없습니다.',
-	INVALID_PASSWORD: '비밀번호가 일치하지 않습니다.',
+	DUPLICATED_EMAIL: {
+		message: '해당 이메일로 가입된 계정이 이미 존재합니다.',
+		status: 409,
+	},
+	EMAIL_NOT_FOUND: {
+		message: '해당 이메일로 가입된 계정이 없습니다.',
+		status: 404,
+	},
+	INVALID_PASSWORD: { message: '비밀번호가 일치하지 않습니다.', status: 401 },
 };
 
 export default AuthErrorMessage;

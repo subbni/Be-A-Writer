2136581816286f716d6972a39946dbd19cced474
subbni <slfkak@naver.com>
2024-09-19 12:22:48 +0900

    [#14] Feat : '글 수정' 관련 리덕스 모듈 작성

--- frontend/src/modules/article/editor/articleEditorActions.js
+++ frontend/src/modules/article/editor/articleEditorActions.js
@@ -1,5 +1,11 @@
 import { createAction } from 'redux-actions';
-import { CHANGE_FIELD, INITIALIZE, WRITE_ARTICLE } from './articleEditorTypes';
+import {
+	CHANGE_FIELD,
+	INITIALIZE,
+	MODIFY_ARTICLE,
+	SET_ORIGINAL_ARTICLE,
+	WRITE_ARTICLE,
+} from './articleEditorTypes';
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -11,3 +17,13 @@ export const writeArticle = createAction(WRITE_ARTICLE, ({ title, subtitle, cont
 	subtitle: subtitle,
 	content: content,
 }));
+export const setOriginalArticle = createAction(SET_ORIGINAL_ARTICLE, (article) => article);
+export const modifyArticle = createAction(
+	MODIFY_ARTICLE,
+	({ articleId, title, subtitle, content }) => ({
+		articleId,
+		title,
+		subtitle,
+		content,
+	}),
+);

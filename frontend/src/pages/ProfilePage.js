35ed5e9f7836e945c2c84b065f0d682c556745b4
subbni <slfkak@naver.com>
2024-10-19 14:16:31 +0900

    [#37] Feat : 프로필 조회 & 변경 UI 구현

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProfileViewContainer from '../containers/profile/ProfileViewContainer';
+
+const ProfilePage = () => {
+	return <ProfileViewContainer />;
+};
+
+export default ProfilePage;

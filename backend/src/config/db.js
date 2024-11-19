dc23d7ab5a0d8d35b42ef96aed3ef868211d9af3
subbni <slfkak@naver.com>
2024-11-19 14:57:20 +0900

    [#41] [BE] Feat : PostgreSQL 설정 파일 환경 변수 적용

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-	user: 'beawriter',
-	host: 'localhost',
-	database: 'beawriter',
-	password: 'beawriter',
-	port: 5432,
+	user: process.env.DB_USER_NAME,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
 });
 
 export default pool;
